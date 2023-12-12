25b5edf17685020a2936402328c06e8c7f997372
Eloxse <122028433+Eloxse@users.noreply.github.com>
2023-12-12 18:41:28 +0100

    Début de Black Sand Atoll
    - Ajout de vidéo

---
+++ Assets/_Script/Islands/IslandOne_Controller.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandOne_Controller : MonoBehaviour
+{
+    #region Variables
+    #endregion
+	
+	#region Properties
+    #endregion
+	
+	#region Builtin Methods
+    void Start()
+    {
+        
+    }
+	#endregion
+}
