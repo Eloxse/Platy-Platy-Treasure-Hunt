37e1dca3a6322d9a1ea76b2d7ee0eda8270d9213
Eloxse <122028433+Eloxse@users.noreply.github.com>
2023-12-13 16:05:51 +0100

    Ile 1 finis
    - Ile 1
    - Scene avec clément finis
    - MEP ile 4

--- Assets/_Script/Islands/IslandOne_Controller.cs
+++ Assets/_Script/Islands/IslandOne_Controller.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IslandOne_Controller : MonoBehaviour
 {
     #region Variables
+    [SerializeField] private float _timeBeforeLoad = 0.5f;
+
+    [SerializeField] private AudioSource _btnSound;
+    [SerializeField] private GameObject _panDialogue1, _panDialogue2, _panDialogue3;
+    [SerializeField] private GameObject _btnChoice1, _btnChoice2;
+    [SerializeField] private GameObject _imgClement, _imgBackground;
     #endregion
 	
 	#region Properties
@@ -15,5 +22,47 @@ public class IslandOne_Controller : MonoBehaviour
     {
         
     }
-	#endregion
+    #endregion
+
+    #region Custom Methods
+    public void NextPannel()
+    {
+        StartCoroutine(DelayNextPannel());
+    }
+
+    private IEnumerator DelayNextPannel()
+    {
+        _btnSound.Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        _panDialogue1.SetActive(false);
+        _panDialogue2.SetActive(true);
+    }
+
+    public void ChoiceOne()
+    {
+        StartCoroutine(DelayChoiceOne());
+    }
+
+    private IEnumerator DelayChoiceOne()
+    {
+        _btnSound.Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        _panDialogue2.SetActive(false);
+        _imgBackground.SetActive(true);
+        _imgClement.SetActive(true);
+        _panDialogue3.SetActive(true);
+    }
+
+    public void ChoiceTwo()
+    {
+        StartCoroutine(DelayChoiceTwo());
+    }
+
+    private IEnumerator DelayChoiceTwo()
+    {
+        _btnSound.Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        SceneManager.LoadScene("Chicken_Isle", LoadSceneMode.Single);
+    }
+    #endregion
 }
