9b14287abd3c50e5653edcec0f73a95ac7d0092f
Eloxse <122028433+Eloxse@users.noreply.github.com>
2023-12-15 12:03:00 +0100

    Boulder Clay
    - load scene
    - Transi + choix
    
    - manque choix 1.2 + 2
    (la fin)

--- Assets/_Script/Islands/Boulder_Clay.cs
+++ Assets/_Script/Islands/Boulder_Clay.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Boulder_Clay : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class Boulder_Clay : MonoBehaviour
     [SerializeField] private AudioSource _btnSound;
     [SerializeField] private GameObject _imgBackground, _imgMap;
     [SerializeField] private GameObject _vid;
-    [SerializeField] private GameObject _panDialogue1, _panDialogue2;
+    [SerializeField] private GameObject _panDialogue1, _panDialogue2, _panDialogue3, _panDialogue4;
     [SerializeField] private GameObject _btnBoulderClay, _btnCannonCove;
     #endregion
 	
@@ -40,6 +41,45 @@ public class Boulder_Clay : MonoBehaviour
         _btnBoulderClay.SetActive(false);
         _btnCannonCove.SetActive(false);
         _vid.SetActive(true);
+        _panDialogue2.SetActive(true);
+    }
+
+    public void DontApproach()
+    {
+        StartCoroutine(DelayDontApproach());
+    }
+
+    private IEnumerator DelayDontApproach()
+    {
+        _btnSound.Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        _panDialogue2.SetActive(false);
+        _panDialogue3 .SetActive(true);
+    }
+
+    public void Wait()
+    {
+        StartCoroutine(DelayWait());
+    }
+
+    private IEnumerator DelayWait()
+    {
+        _btnSound.Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        _panDialogue2 .SetActive(false);
+        _panDialogue4 .SetActive(true);
+    }
+
+    public void BlackSand()
+    {
+        StartCoroutine(DelayBlackSand());
+    }
+
+    private IEnumerator DelayBlackSand()
+    {
+        _btnSound .Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        SceneManager.LoadScene("Black_Sand_Atoll", LoadSceneMode.Single);
     }
     #endregion
 }
