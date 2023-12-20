1085c8989ed914cea2c47f7f839c1c1356747f96
Eloxse <122028433+Eloxse@users.noreply.github.com>
2023-12-20 22:38:32 +0100

    Black Water Cave
    - Debut dialogue
    - Debut choix

--- Assets/_Script/Islands/Black_Water.cs
+++ Assets/_Script/Islands/Black_Water.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Black_Water : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    #region Variables
+    [SerializeField] private float _timeBeforeLoad = 0.5f;
+
+    [SerializeField] private AudioSource _btnSound;
+    [SerializeField] private GameObject _panDialogue2, _panDialogue3, _panDialogue4;
+    #endregion
+
+    #region Custom Methods
+    public void Refuling(){
+        StartCoroutine(DelayRefuling());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private IEnumerator DelayRefuling(){
+        _btnSound.Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        _panDialogue2.SetActive(false);
+        _panDialogue3.SetActive(true);
     }
+    
+    public void Arise(){
+        StartCoroutine(DelayArise());
+    }
+
+    private IEnumerator DelayArise(){
+        _btnSound.Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        _panDialogue2.SetActive(false);
+        _panDialogue4.SetActive(true);
+    }
+    #endregion
 }
