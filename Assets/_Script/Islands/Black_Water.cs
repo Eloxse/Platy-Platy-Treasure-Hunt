62078f90f3c1152fc6c21d645aca4b606458bebc
Eloxse <122028433+Eloxse@users.noreply.github.com>
2023-12-24 00:42:55 +0100

    Dev de l'histoire finis

--- Assets/_Script/Islands/Black_Water.cs
+++ Assets/_Script/Islands/Black_Water.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Black_Water : MonoBehaviour
 {
@@ -8,7 +9,8 @@ public class Black_Water : MonoBehaviour
     [SerializeField] private float _timeBeforeLoad = 0.5f;
 
     [SerializeField] private AudioSource _btnSound;
-    [SerializeField] private GameObject _panDialogue2, _panDialogue3, _panDialogue4;
+    [SerializeField] private GameObject _panDialogue2, _panDialogue3, _panDialogue4, _panDialogue5, _panDialogue6;
+    [SerializeField] private GameObject _imgBackground, _imgMegalodon, _imgKiller;
     #endregion
 
     #region Custom Methods
@@ -32,6 +34,42 @@ public class Black_Water : MonoBehaviour
         yield return new WaitForSeconds(_timeBeforeLoad);
         _panDialogue2.SetActive(false);
         _panDialogue4.SetActive(true);
+    }
+    
+    public void Megalodon(){
+        StartCoroutine(DelayMegalodon());
+    }
+
+    private IEnumerator DelayMegalodon(){
+        _btnSound.Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        _panDialogue3.SetActive(false);
+        _panDialogue5.SetActive(true);
+        _imgBackground.SetActive(true);
+        _imgMegalodon.SetActive(true);
+    }
+    
+    public void Count(){
+        StartCoroutine(DelayCount());
+    }
+
+    private IEnumerator DelayCount(){
+        _btnSound.Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        SceneManager.LoadScene("Blind_Mans_Lagoon", LoadSceneMode.Single);
+    }
+    
+    public void Killer(){
+        StartCoroutine(DelayKiller());
+    }
+
+    private IEnumerator DelayKiller(){
+        _btnSound.Play();
+        yield return new WaitForSeconds(_timeBeforeLoad);
+        _panDialogue4.SetActive(false);
+        _panDialogue6.SetActive(true);
+        _imgBackground.SetActive(true);
+        _imgKiller.SetActive(true);
     }
     #endregion
 }
